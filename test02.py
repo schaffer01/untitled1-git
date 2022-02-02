8bfa17be980798b093f36f17ba7555d2da820b1b
zhang <3257194259@qq.com>
2022-02-02 20:52:06 +0800

    xxx

--- test02.py
+++ test02.py
@@ -4,3 +4,6 @@ print(re.findall(r'a*', 'a.jpf'))
 print(b'ok')
 
 print('yes')
+"git remote add origin git@github.com:schaffer01/untitled1-git.git\
+git push -u origin main \
+git remote add origin git@github.com:schaffer01/untitled1-git.git"
